8194500185d5910810d4fff355834eb0ba93428e
Sparsh Tyagi <138455932+sparshtyagi1811@users.noreply.github.com>
2024-06-14 15:54:56 +0530

    Add files via upload

---
+++ Task1a/script.js
@@ -0,0 +1,9 @@
+document.getElementById('projectForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Project Details Submitted');
+});
+
+document.getElementById('projectTypeForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Project Type Submitted');
+});
